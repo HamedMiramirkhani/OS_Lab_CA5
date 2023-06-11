8a6c3fdc7b8a6581e81733e41f7c2c61fc720f96
Hamed_Miramirkhani <Hamedkhan8181@yahoo.com>
2023-06-11 06:04:24 +0330

    Define st_sz(stack size) in proc struct

--- codes/exec.c
+++ codes/exec.c
@@ -64,12 +64,21 @@ exec(char *path, char **argv)
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  
   // Use the second as the user stack.
+ 
+  /* COMMENT following
   sz = PGROUNDUP(sz);
-  
   if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
     goto bad;
   clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
   sp = sz;
+  */
+
+  uint stack_top = STACKTOP;
+  if((stack_top = allocuvm(pgdir, stack_top-PGSIZE, stack_top)) == 0)
+		 goto bad;
+  sp = stack_top;
+  uint st_sz = 1;
+
   // Push argument strings, prepare rest of stack in ustack.
   for(argc = 0; argv[argc]; argc++) {
     if(argc >= MAXARG)
@@ -102,6 +111,7 @@ exec(char *path, char **argv)
   curproc->sz = sz;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
+  curproc->st_sz = st_sz;
   switchuvm(curproc);
   freevm(oldpgdir);
   return 0;
